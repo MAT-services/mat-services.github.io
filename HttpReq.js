572847e3620c76fafe70ffe19a7e0c7eb91fc4b0
MAT Services <157519566+MAT-services@users.noreply.github.com>
2025-05-27 18:50:44 +0000

    test

--- HttpReq.js
+++ HttpReq.js
@@ -3,6 +3,7 @@ function includeHTML(elementId, file) {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById(elementId).innerHTML = this.responseText;
+            if (typeof callback === "function") callback();
     }
   };
   xhttp.open("GET", file, true);
